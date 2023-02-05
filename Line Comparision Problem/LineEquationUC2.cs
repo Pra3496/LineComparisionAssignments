56bc704b3a25baf211593be0895353a268cb290a
Pranav Waghmare <piyushw55@gmail.com>
2023-02-05 14:28:56 +0530

    As a fan of geometry, I want to check equality of two lines based on the end points, So that I know when two lines are the equal.

---
+++ Line Comparision Problem/LineEquationUC2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparision_Problem
+{
+    internal class LineEquationUC2
+    {
+
+        public bool EquationCmp(double X1, double X2, double X3, double X4, double Y1, double Y2, double Y3, double Y4)
+        {
+            double temp1 = Math.Pow((X2 - X1), 2);
+            double temp2 = Math.Pow((Y2 - Y1), 2);
+            double Length1 = Math.Sqrt(temp1 + temp2);
+            Console.WriteLine("\nLength of line1: " + Length1);
+
+            double temp3 = Math.Pow((X4 - X3), 2);
+            double temp4 = Math.Pow((Y4 - Y3), 2);
+            double Length2 = Math.Sqrt(temp3 + temp4);
+            Console.WriteLine("\nLength of line2: " + Length2);
+
+            if (Length1.Equals(Length2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+        }
+    }
+}
--- Line Comparision Problem/Program.cs
+++ Line Comparision Problem/Program.cs
@@ -1,25 +1,53 @@
-﻿namespace Line_Comparision_Problem
+﻿using System.Diagnostics.SymbolStore;
+
+namespace Line_Comparision_Problem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcomw! to Line Comparison Pragram");
-            Console.WriteLine("Please enter X1 coordinates");
+            
+            Console.WriteLine("\n\nWelcome! to Line Comparison Pragram\n");
+
+            Console.Write("\nFirst Line\n");
+            Console.Write("\nPlease enter X1 coordinates : ");
             var X1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please enter Y1 coordinates");
+            Console.Write("\nPlease enter Y1 coordinates : ");
             var Y1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Please enter X2 coordinates");
+            Console.Write("\nPlease enter X2 coordinates : ");
             var X2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please enter Y2 coordinates");
+            Console.Write("\nPlease enter Y2 coordinates : ");
             var Y2 = Convert.ToDouble(Console.ReadLine());
 
-            LineEquationUC1 le = new LineEquationUC1();
+            Console.Write("\n\nSecond Line\n");
+            Console.Write("\nPlease enter X3 coordinates : ");
+            var X3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nPlease enter Y3 coordinates : ");
+            var Y3 = Convert.ToDouble(Console.ReadLine());
 
-            double result = le.Equation(X1,X2,Y1,Y2);
 
-            Console.WriteLine("\nThe length of line is : " + result);
+            Console.Write("\nPlease enter X4 coordinates : ");
+            var X4 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nPlease enter Y4 coordinates : ");
+            var Y4 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\n\n");
+
+            LineEquationUC2 le = new LineEquationUC2();
+
+            bool chk = le.EquationCmp(X1,X2,X3,X4,Y1,Y2,Y3,Y4);
+
+            if (chk == true)
+            {
+                Console.WriteLine("\n\n\tLines are equal\n");
+            }
+            else
+            {
+                Console.WriteLine("\n\n\tLines are not equal\n");
+            }
+
+
+
         }
     }
 }
