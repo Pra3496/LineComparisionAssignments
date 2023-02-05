781e6fa295c3e081a66a2510a9f2c3e151459012
Pranav Waghmare <piyushw55@gmail.com>
2023-02-05 13:35:45 +0530

    As a fan of geometry, I want to model a line based on a point consisting of (x, y) co-ordinates using the Cartesian system, So that I can calculate its length.

---
+++ Line Comparision Problem/LineEquationUC1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparision_Problem
+{
+    internal class LineEquationUC1
+    {
+
+        public double Equation(double X1, double X2, double Y1, double Y2)
+        {
+            double temp1 = Math.Pow((X2 - X1), 2);
+            double temp2 = Math.Pow((Y2 - Y1), 2);
+            return Math.Sqrt(temp1 + temp2);
+
+        }
+        
+
+
+    }
+}
---
+++ Line Comparision Problem/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Line_Comparision_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcomw! to Line Comparison Pragram");
+            Console.WriteLine("Please enter X1 coordinates");
+            var X1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter Y1 coordinates");
+            var Y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter X2 coordinates");
+            var X2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter Y2 coordinates");
+            var Y2 = Convert.ToDouble(Console.ReadLine());
+
+            LineEquationUC1 le = new LineEquationUC1();
+
+            double result = le.Equation(X1,X2,Y1,Y2);
+
+            Console.WriteLine("\nThe length of line is : " + result);
+        }
+    }
+}
